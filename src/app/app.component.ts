beb80b428d218fd741f942d965003ab40041cf2c
rband <rombander@gmail.com>
2019-12-10 13:45:39 +0200

    create comment app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test-app';
+  value = '';
+  comments = [
+    {
+      comment: 'test commenttest commenttest commenttest commenttest commenttest commenttest commenttest commenttest commenttest commenttest commenttest commenttest commenttest commenttest commenttest commenttest commenttest comment',
+      subСomments: ['test sub comment test sub comment test sub comment test sub commentv test sub comment v vtest sub commentv vvtest sub commentv vtest sub comment test sub commenttest sub comment']
+    },
+    {
+      comment: 'test comment2, test comment2 test comment2 test comment2 test comment2 vv v vv test comment2test comment2vtest comment2v vv test comment2 v v vv vtest comment2test comment2v vtest comment2v vv',
+      subСomments: []
+    }
+  ]
+
+  addComment(comment) {
+    if (comment) {
+      this.comments.push({comment: comment, subСomments: []});
+      this.value = '';
+    }
+  }
+
+  addSubComment(subComment, i) {
+    if (subComment) {
+      this.comments[i].subСomments.push(subComment);
+    }
+  }
+
+  removeComment(i) {
+    this.comments.splice(i, 1);
+  }
+
+  removeSubComment(i, j) {
+    this.comments[i].subСomments.splice(j, 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
+import { FormsModule }   from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    Ng2SearchPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
